50acc6a37f5c3c991fa275c70b08c4f7e56def4f
Asadbek <kenjabaev.asadbek@gmil.com>
2022-09-08 12:28:00 +0500

    validation passwords and data exist

--- app.js
+++ app.js
@@ -4,18 +4,38 @@ const email = document.querySelector('#email');
 const username = document.querySelector('#username');
 const newPassword = document.querySelector('#newPassword');
 const confirmPassword = document.querySelector('#confirmPassword');
+const errorMsg = document.querySelector('#errorMsg');
 
 
 
 
 
 
-submitBtn.addEventListener('click', ()=>{
+submitBtn.addEventListener('click', ($event)=>{
+    $event.defaultPrevented();
     if(firstName.value && email.value && username.value && newPassword.value && confirmPassword.value){
         alert(`Congratulations ${firstName.value}! You have successfully registered`)
     }else{
         alert('Missing information. Please, fill out required fields');
     }
-
 })
+
+// Checking if both password field is match
+
+confirmPassword.addEventListener('blur', ()=>{
+    if(newPassword.value === confirmPassword.value){
+        newPassword.style.border = 'thin solid green';
+        confirmPassword.style.border = 'thin solid green';
+    }else{
+        newPassword.style.border = 'thin solid red';
+        confirmPassword.style.border = 'thin solid red';
+        errorMsg.style.display = 'inline';
+        errorMsg.style.color = 'red';
+    }
+})
+
+confirmPassword.addEventListener('focus', ()=>{
+    errorMsg.style.display = 'none';
+})
+
 
